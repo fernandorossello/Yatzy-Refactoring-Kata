cfa0894af3ce4d47846eada38d2fbf7ece4348ef
Fernando Rosselló <fmrossello@gmail.com>
2022-06-15 22:19:14 +0200

    Renaming methods and simplifying tests

--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -1,112 +1,108 @@
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class YatzyTest {
 
     @Test
     public void test_chance_shouldScoreTheSumOfAllDices() {
-        int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
-        assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
+        assertEquals(15, Yatzy.chance(2, 3, 4, 5, 1));
+        assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
     }
 
-    @Test public void test_yatzy_shouldScore50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+    @Test
+    public void test_yatzy_shouldScore50() {
+        assertEquals(50, Yatzy.yatzy(4, 4, 4, 4, 4));
+        assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
+        assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3));
     }
 
-    @Test public void test_ones_shouldSumAllDicesWithValueOne() {
-        assertTrue(Yatzy.ones(1,2,3,4,5) == 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+    @Test
+    public void test_ones_shouldSumAllDicesWithValueOne() {
+        assertEquals(1, Yatzy.ones(1, 2, 3, 4, 5));
+        assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
+        assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
+        assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
     }
 
     @Test
     public void test_twos_shouldSumAllDicesWithValueTwo() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
+        assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
+        assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
     }
 
     @Test
     public void test_threes_shouldSumAllDicesWithValueThree() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
+        assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
+        assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
     }
 
     @Test
-    public void test_fours_shouldSumAllDicesWithValueFour()
-    {
-        //TODO: Why in this case they instantiate it?
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+    public void test_fours_shouldSumAllDicesWithValueFour() {
+        assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
+        assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
+        assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
     }
 
     @Test
     public void test_fives_shouldSumAllDicesWithValueFive() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+        assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
+        assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
+        assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
     }
 
     @Test
     public void test_sixes_shouldSumAllDicesWithValueSix() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+        assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
+        assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
+        assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
     }
 
     @Test
     public void test_onePair_shouldSumTheValueOfThePair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+        assertEquals(6, Yatzy.onePair(3, 4, 3, 5, 6));
+        assertEquals(10, Yatzy.onePair(5, 3, 3, 3, 5));
+        assertEquals(12, Yatzy.onePair(5, 3, 6, 6, 5));
     }
 
     @Test
     public void test_twoPairs_shouldSumTheValueOfTheTwoPairs() {
-        assertEquals(16, Yatzy.two_pair(3,3,5,4,5));
-        assertEquals(16, Yatzy.two_pair(3,3,5,5,5));
+        assertEquals(16, Yatzy.twoPairs(3, 3, 5, 4, 5));
+        assertEquals(16, Yatzy.twoPairs(3, 3, 5, 5, 5));
     }
 
     @Test
-    public void test_threeOfAKind_shouldSumAllValuesOfTheThreeOfAKind()
-    {
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,4,5));
-        assertEquals(15, Yatzy.three_of_a_kind(5,3,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,5));
+    public void test_threeOfAKind_shouldSumAllValuesOfTheThreeOfAKind() {
+        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 4, 5));
+        assertEquals(15, Yatzy.threeOfAKind(5, 3, 5, 4, 5));
+        assertEquals(9, Yatzy.threeOfAKind(3, 3, 3, 3, 5));
     }
 
     @Test
     public void test_fourOfaAKind_shouldSumAllValuesOfTheFourOfAKind() {
-        assertEquals(12, Yatzy.four_of_a_kind(3,3,3,3,5));
-        assertEquals(20, Yatzy.four_of_a_kind(5,5,5,4,5));
-        assertEquals(9, Yatzy.three_of_a_kind(3,3,3,3,3));
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 5));
+        assertEquals(20, Yatzy.fourOfAKind(5, 5, 5, 4, 5));
+        assertEquals(12, Yatzy.fourOfAKind(3, 3, 3, 3, 3));
     }
 
     @Test
     public void test_smallWtraight_shouldScoreFifteen() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
+        assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
+        assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
+        assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
     }
 
     @Test
     public void test_largeStraight_shouldScoreTwenty() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
+        assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
+        assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
+        assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
     }
 
     @Test
     public void test_fullHouse_ShouldScoreTheSumOfAllTheValues() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(22, Yatzy.fullHouse(6,2,2,6,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+        assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
+        assertEquals(22, Yatzy.fullHouse(6, 2, 2, 6, 6));
+        assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
     }
 }
