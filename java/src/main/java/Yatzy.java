1eda570e07cfdc86494c65fb7b3182b6773120af
Fernando Rosselló <fmrossello@gmail.com>
2022-06-16 22:22:12 +0200

    Refactor ones method to make it not static

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -15,8 +15,8 @@ public class Yatzy {
         return Arrays.stream(rolledDices).sum();
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        return sumDicesWithValue(1, d1, d2, d3, d4, d5);
+    public int ones() {
+        return sumDicesWithValue(1, this.rolledDices);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
